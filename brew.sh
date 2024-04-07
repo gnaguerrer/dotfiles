052da8a79c45cfdcd58f8c47b646a854ba098455
Gina Guerrero <gnaguerrer@gmail.com>
2024-04-07 11:14:11 -0500

    add brew.sh and update aliases

---
+++ brew.sh
@@ -0,0 +1,11 @@
+echo "Installing Screen Copy (scrcpy)..."
+brew install scrcpy
+
+echo "Installing android-platform-tools..."
+brew install android-platform-tools
+
+echo "Installing Node Version Manager..."
+brew install nvm
+
+echo "Installing ngrok..."
+brew install ngrok/ngrok/ngrok
