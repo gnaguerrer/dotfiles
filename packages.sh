cc6871e8f988794ef6d96116beadd3bc04fdf06f
Gina Guerrero <gnaguerrer@gmail.com>
2025-03-31 09:11:45 -0500

    add packages

---
+++ packages.sh
@@ -0,0 +1,23 @@
+echo "Installing Node"
+brew install node
+
+echo "Installing Yarn..."
+npm install --global yarn
+
+echo "Installing Node Version Manager..."
+brew install nvm
+
+echo "Installing Screen Copy (scrcpy)..."
+brew install scrcpy
+
+echo "Installing android-platform-tools..."
+brew install android-platform-tools
+
+# echo "Installing ngrok..."
+# brew install ngrok/ngrok/ngrok
+
+echo "Installing chruby ruby-install..."
+brew install chruby ruby-install
+
+echo "Installing visual-studio-code..."
+brew install --cask visual-studio-code
