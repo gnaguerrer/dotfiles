67eed7d8aea98f6a8af8f5867e6f2470e11aa333
Gina Guerrero <gnaguerrer@gmail.com>
2025-04-02 08:45:39 -0500

    adjust brew.sh

--- brew.sh
+++ brew.sh
@@ -1,11 +1,4 @@
-echo "Installing Screen Copy (scrcpy)..."
-brew install scrcpy
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo "Installing android-platform-tools..."
-brew install android-platform-tools
-
-echo "Installing Node Version Manager..."
-brew install nvm
-
-echo "Installing ngrok..."
-brew install ngrok/ngrok/ngrok
